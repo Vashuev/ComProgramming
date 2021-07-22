5d5e741b9dcbc682203399dfd9e0802bc48aa9fb
Rohit Bisht <rohitbst.104764@gmail.com>
2021-07-22 12:50:43 +0530

    login url changed

--- questionList/urls.py
+++ questionList/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.registerPage, name='register'),
-    path('login/', views.loginPage, name='login'),
+    path('', views.loginPage, name='login'),
     path('logout/', views.logoutPage, name='logout'),
 
     path('questionslist/', views.questions_list, name="questions"),
